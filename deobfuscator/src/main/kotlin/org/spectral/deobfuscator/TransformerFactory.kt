932d34a4d3fabfd98fdca7739d1409e658af77a3
kyle-escobar <kslrtips@gmail.com>
2020-09-07 19:48:09 -0400

    Remove static field inliner

--- deobfuscator/src/main/kotlin/org/spectral/deobfuscator/TransformerFactory.kt
+++ deobfuscator/src/main/kotlin/org/spectral/deobfuscator/TransformerFactory.kt
@@ -1,7 +1,6 @@
 package org.spectral.deobfuscator
 
 import org.spectral.deobfuscator.transform.RuntimeExceptionRemover
-import org.spectral.deobfuscator.transform.StaticFieldInliner
 import org.spectral.deobfuscator.transform.controlflow.ControlFlowFixer
 
 /**
@@ -14,8 +13,7 @@ import org.spectral.deobfuscator.transform.controlflow.ControlFlowFixer
 enum class TransformerFactory(val order: Int, val build: () -> Transformer) {
 
     RUNTIME_EXCEPTION(1, { RuntimeExceptionRemover() }),
-    CONTROL_FLOW(2, { ControlFlowFixer() }),
-    STATIC_FIELD_INLINER(3, { StaticFieldInliner() });
+    CONTROL_FLOW(2, { ControlFlowFixer() });
 
     companion object {
         val values = enumValues<TransformerFactory>()
