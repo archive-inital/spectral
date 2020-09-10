f37b974af1bee9ea47524c276b9d135dd4565e54
kyle-escobar <kslrtips@gmail.com>
2020-09-09 23:22:12 -0400

    Finish annotation processor

--- asm/processor/src/main/kotlin/org/spectral/asm/processor/InstructionDataProcessor.kt
+++ asm/processor/src/main/kotlin/org/spectral/asm/processor/InstructionDataProcessor.kt
@@ -1,120 +1,85 @@
 package org.spectral.asm.processor
 
-import com.google.auto.common.BasicAnnotationProcessor
-import com.google.auto.service.AutoService
-import com.google.common.collect.SetMultimap
+import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import org.spectral.asm.annotation.InstructionData
 import java.io.File
-import java.io.IOException
-import javax.annotation.processing.Messager
-import javax.annotation.processing.Processor
+import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.Element
-import javax.lang.model.element.ElementKind
 import javax.lang.model.element.TypeElement
-import javax.lang.model.util.Elements
+import javax.lang.model.type.MirroredTypeException
+import javax.lang.model.type.TypeMirror
 import javax.tools.Diagnostic
+import kotlin.reflect.KClass
 
 /**
  * The primary annotation processor class which processes any '@Instruction' annotations.
  *
  * These are used to generate a 'InstructionType.kt' enum class dynamically during compile time.
  */
-class InstructionDataProcessor : BasicAnnotationProcessor() {
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
+@SupportedAnnotationTypes("org.spectral.asm.annotation.InstructionData")
+@SupportedOptions(InstructionDataProcessor.KAPT_KOTLIN_GENERATED_OPTION_NAME)
+class InstructionDataProcessor : AbstractProcessor() {
 
-    override fun getSupportedSourceVersion(): SourceVersion = SourceVersion.latestSupported()
+    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment): Boolean {
+        val annotatedElements = roundEnv.getElementsAnnotatedWith(InstructionData::class.java)
+        if(annotatedElements.isEmpty()) return false
 
-    override fun getSupportedOptions(): Set<String> = setOf(KAPT_KOTLIN_GENERATED_OPTION_NAME)
+        val kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME] ?: run {
+            processingEnv.messager.printMessage(Diagnostic.Kind.ERROR, "Can not find target directory for generated kotlin files.")
+            return false
+        }
 
-    override fun initSteps(): Iterable<ProcessingStep> {
-        val outputDirectory = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]?.let { File(it) }
-            ?: throw IllegalArgumentException("No output directory specified.")
+        val enum = TypeSpec.enumBuilder("InstructionType")
+            .primaryConstructor(
+                FunSpec.constructorBuilder()
+                    .addParameter("opcode", Int::class)
+                    .addParameter("name", String::class)
+                    .addParameter("type", KClass::class.asClassName().parameterizedBy(TypeVariableName("*")))
+                    .build()
+            )
 
-        return listOf(InstructionDataProcessingStep(
-            elements = processingEnv.elementUtils,
-            messager = processingEnv.messager,
-            outputDir = outputDirectory
-        ))
+        for(element in annotatedElements) {
+            val typeElement = element.toTypeElementOrNull() ?: continue
+            val annotation = element.getAnnotation(InstructionData::class.java)
+
+            enum.addEnumConstant(annotation.name.toUpperCase(), TypeSpec.anonymousClassBuilder()
+                .addSuperclassConstructorParameter("%L", annotation.opcode)
+                .addSuperclassConstructorParameter("%S", annotation.name)
+                .addSuperclassConstructorParameter("%T::class", ClassName("org.spectral.asm.code.instruction", typeElement.simpleName.toString()))
+                .build())
+        }
+
+        FileSpec.builder("org.spectral.asm.code", "InstructionType")
+            .addType(enum.build())
+            .build()
+            .writeTo(File(kaptKotlinGeneratedDir))
+
+        return true
+    }
+
+    fun Element.toTypeElementOrNull(): TypeElement? {
+        if(this !is TypeElement) {
+            processingEnv.messager.printMessage(Diagnostic.Kind.ERROR, "Invalid element type, class expected.", this)
+            return null
+        }
+
+        return this
+    }
+
+    fun TypeElement.toMirror(): TypeMirror {
+        try {
+            this.asType()
+        } catch (e : MirroredTypeException) {
+            return e.typeMirror
+        }
+
+        throw Exception("Failed")
     }
 
     companion object {
-        /**
-         * The kapt generated source files sourceSet name.
-         */
-        private const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
-    }
-}
-
-/**
- * The annotation processing step that runs for each found annotation
- * requiring attention.
- *
- * @property elements Elements
- * @property messager Messager
- * @property outputDir File
- * @constructor
- */
-class InstructionDataProcessingStep(
-    private val elements: Elements,
-    private val messager: Messager,
-    private val outputDir: File
-) : BasicAnnotationProcessor.ProcessingStep {
-
-    private val instructionClasses = LinkedHashMap<String, InstructionDataGroupedClasses>()
-
-    override fun annotations() = setOf(InstructionData::class.java)
-
-    override fun process(elementsByAnnotation: SetMultimap<Class<out Annotation>, Element>): Set<Element> {
-        try {
-
-            /*
-             * Processing Start.
-             */
-            for(annotatedElement in elementsByAnnotation[InstructionData::class.java]) {
-                if(annotatedElement.kind != ElementKind.CLASS) {
-                    throw ProcessingException(annotatedElement,
-                        "Only classes can be annotated with @${InstructionData::class.java}")
-                }
-
-                val typeElement = annotatedElement as TypeElement
-                val annotatedClass = InstructionDataAnnotatedClass(typeElement)
-
-                var instructionClass = instructionClasses[annotatedClass.name]
-                if(instructionClass == null) {
-                    val name = annotatedClass.name
-                    instructionClass = InstructionDataGroupedClasses(name)
-                    instructionClasses[name] = instructionClass
-                }
-
-                instructionClass += annotatedClass
-
-            }
-
-            /*
-             * Generate source.
-             */
-            for(instructionClass in instructionClasses.values) {
-                instructionClass.generateCode(elements, outputDir)
-            }
-
-            instructionClasses.clear()
-
-        } catch(e : ProcessingException) {
-            error(e.element, e.message)
-        } catch(e : IOException) {
-            error(null, e.message)
-        }
-
-        return emptySet()
-    }
-
-    /**
-     * Prints an annotation processing error to the console.
-     *
-     * @param e Element?
-     * @param message String?
-     */
-    private fun error(e: Element?, message: String?) {
-        messager.printMessage(Diagnostic.Kind.ERROR, message, e)
+        const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
     }
 }
--- asm/processor/src/main/kotlin/org/spectral/asm/processor/ProcessingException.kt
+++
@@ -1,5 +0,0 @@
-package org.spectral.asm.processor
-
-import javax.lang.model.element.Element
-
-class ProcessingException(val element: Element, message: String) : RuntimeException(message)
