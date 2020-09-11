3a05fcfb21905af271cafbef1829352513f09943
kyle-escobar <kslrtips@gmail.com>
2020-09-11 15:25:24 -0400

    Add jar loading

--- asm/src/main/kotlin/org/spectral/asm/ClassPool.kt
+++ asm/src/main/kotlin/org/spectral/asm/ClassPool.kt
@@ -1,5 +1,10 @@
 package org.spectral.asm
 
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.tree.ClassNode
+import java.io.File
+import java.util.jar.JarFile
+
 class ClassPool {
 
     private val classMap = hashMapOf<String, Class>()
@@ -18,5 +23,25 @@ class ClassPool {
 
     operator fun get(name: String): Class? = classMap[name]
 
+    companion object {
 
+        fun loadJar(file: File): ClassPool {
+            val pool = ClassPool()
+
+            JarFile(file).use { jar ->
+                jar.entries().asSequence()
+                    .filter { it.name.endsWith(".class") }
+                    .forEach {
+                        val node = ClassNode()
+                        val reader = ClassReader(jar.getInputStream(it))
+
+                        reader.accept(node, ClassReader.SKIP_FRAMES)
+
+                        pool.add(Class(pool, node))
+                    }
+            }
+
+            return pool
+        }
+    }
 }
