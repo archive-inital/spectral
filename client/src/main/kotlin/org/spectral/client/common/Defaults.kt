9b0648e0ff60da0787db16edddb6ab1dde234843
kyle-escobar <kslrtips@gmail.com>
2020-09-11 23:17:55 -0400

    Add platform information enum

---
+++ client/src/main/kotlin/org/spectral/client/common/Defaults.kt
@@ -0,0 +1,17 @@
+package org.spectral.client.common
+
+/**
+ * Defaults which could be configuration settings options
+ * however they are internal to the client's code so we just define them
+ * here.
+ */
+object Defaults {
+
+    /**
+     * The root Spectral data folder name.
+     *
+     * A prefix is appended to this path name depending
+     * on the users running platform.
+     */
+    const val SPECTRAL_DIR = "spectral/"
+}
