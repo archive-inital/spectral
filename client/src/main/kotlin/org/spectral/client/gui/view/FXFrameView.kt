e9fda3d1d746ea0b1686421f081fa789263700f3
kyle-escobar <kslrtips@gmail.com>
2020-09-13 21:29:47 -0400

    Style window

--- client/src/main/kotlin/org/spectral/client/gui/view/FXFrameView.kt
+++ client/src/main/kotlin/org/spectral/client/gui/view/FXFrameView.kt
@@ -1,6 +1,7 @@
 package org.spectral.client.gui.view
 
 import javafx.scene.image.Image
+import javafx.scene.layout.Priority
 import javafx.scene.layout.Region
 import javafx.scene.text.Font
 import org.spectral.client.gui.Gui
@@ -10,18 +11,39 @@ class FXFrameView : View() {
 
     private val gui: Gui by di()
 
-    override val root = vbox(50) {
-        paddingAll = 6.0
-        hbox(8) {
-            imageview(Image("/spectral-app.png")) {
-                fitWidth = 18.0
-                fitHeight = 18.0
-            }
+    override val root = vbox(0) {
+        borderpane {
+            left = hbox(8) {
+                    paddingTop = 6.0
+                    paddingLeft = 6.0
+                    paddingRight = 6.0
 
-            label("Spectral") {
-                font = Font(13.0)
-                style = "-fx-font-weight: bold;"
+                    imageview(Image("/spectral-app.png")) {
+                        fitWidth = 18.0
+                        fitHeight = 18.0
+                    }
+
+                    label("Spectral") {
+                        font = Font(13.0)
+                        style = "-fx-font-weight: bold;"
+                    }
+                }
+
+            right = hbox(4) {
+                paddingTop = 6.0
+                paddingLeft = 6.0
+                paddingRight = 6.0
+
+                button("X")
             }
+        }
+
+        menubar {
+            paddingTop = 8.0
+            menu("File")
+            menu("Edit")
+            menu("Plugins")
+            menu("Tools")
         }
     }
 }
